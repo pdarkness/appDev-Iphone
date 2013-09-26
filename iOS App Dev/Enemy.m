0bf89f79b188ad5fa475eff529f41092aeaef6d6
Knútur Óli Magnússon <knuturoli@Gogo-Magnusdottirs-MacBook-Pro.local>
2013-09-26 14:02:09 +0000

    Enemy hotbox update

--- iOS App Dev/Enemy.m
+++ iOS App Dev/Enemy.m
@@ -11,12 +11,14 @@
 @implementation Enemy
 
 - (id)initWithSpace:(ChipmunkSpace *)space position:(CGPoint)position{
-    self = [super initWithFile:@"AppleSprite3.gif"];
+    self = [super initWithFile:@"AppleSprite4.gif"];
     if (self)  {
         CGSize size = self.textureRect.size;
         ChipmunkBody *body = [ChipmunkBody staticBody];
+        position.x -= 20;
         body.pos = position;
-        ChipmunkShape *shape = [ChipmunkPolyShape boxWithBody:body width:size.width height:size.height/8];
+        ChipmunkShape *shape = [ChipmunkPolyShape boxWithBody:body width:size.width/1.5 height:size.height/1.5];
+
         shape.sensor = YES;
         
         [space addShape:shape];
