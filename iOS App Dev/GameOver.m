b8a95cd64247c3cbee41b76918bb5811443bf557
elisaerlu <elisae11@ru.is>
2013-09-29 22:03:14 +0000

    some refactor job done, YouWon.h and .m files been removed. GameOver.h and .m files take care of the everything

--- iOS App Dev/GameOver.m
+++ iOS App Dev/GameOver.m
@@ -11,24 +11,31 @@
 #import "Game.h"
 
 @implementation GameOver
-- (id)init
+
+-(id)initGameOver:(NSInteger)playerScore :(BOOL)won
 {
-    self = [super init];
-    if(self != nil)
-    {
-        CCLabelTTF *label = [CCLabelTTF labelWithString:@"GAME OVER! \nPLAY AGAIN" fontName:@"Arial" fontSize:48];
+    if ((self = [super init])) {
+        NSString *message;
+        if (won) {
+            message = [NSString stringWithFormat:@"Congratulations my friend you won\n You scored %d points\nPlay Again", playerScore];
+        } else {
+            message = [NSString stringWithFormat:@"Sorry my friend you lost\n You scored %d points\nPlay Again", playerScore];
+        }
+        CGSize winSize = [CCDirector sharedDirector].winSize;
+        CCLabelTTF *label = [CCLabelTTF labelWithString:message fontName:@"Arial" fontSize:22];
+        
+        
         CCMenuItemLabel *button = [CCMenuItemLabel itemWithLabel:label block:^(id sender)
                                    {
                                        Game *game = [[Game alloc] init];
                                        [[CCDirector sharedDirector] replaceScene:game];
                                    }];
-        button.position = ccp(200, 200);
+        button.position = ccp(winSize.width / 2, winSize.height / 2);
         
         CCMenu *menu = [CCMenu menuWithItems:button, nil];
         menu.position = CGPointZero;
         [self addChild:menu];
     }
-    
     return self;
 }
 
